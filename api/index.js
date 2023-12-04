3bcfb977415575280eea1165f9ef5666804fd704
Battarasetty <Harish93601@gmail.com>
2023-12-04 16:08:30 +0530

    Added Update User info api

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen('5555', () => {
     console.log(`server is running at port 5555!!`);
@@ -29,6 +31,6 @@ app.use((err, req, res, next) => {
     res.status(statusCode).json({
         success: false,
         statusCode,
-        message, 
+        message,
     })
 })
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
-import express from "express";
-import { test } from "../controllers/user.controller.js";
+import express from 'express';
+import { test, updateUser} from '../controllers/user.controller.js';
+import { verifyUser } from '../utilis/verifyUser.js';
 
 
 const router = express.Router();
 
 router.get('/', test)
-
+router.post('/update/:id', verifyUser, updateUser)
 
 export default router;
