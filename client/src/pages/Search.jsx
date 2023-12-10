7b6cca2e0d5c1c4417eee921056533c69ca4d576
Battarasetty <Harish93601@gmail.com>
2023-12-10 12:47:00 +0530

    Listing Item component changes added

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -16,7 +16,6 @@ export default function Search() {
 
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
-    console.log(listings);
     const [showMore, setShowMore] = useState(false);
 
     useEffect(() => {
@@ -55,6 +54,7 @@ export default function Search() {
             const searchQuery = urlParams.toString();
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json();
+            console.log(data);
             if (data.length > 8) {
                 setShowMore(true);
             } else {
@@ -115,9 +115,7 @@ export default function Search() {
         navigate(`/search?${searchQuery}`);
     };
 
-    const onShowMoreClick = async () => {
-        
-    };
+
     return (
         <div className='flex flex-col md:flex-row'>
             <div className='p-7  border-b-2 md:border-r-2 md:min-h-screen'>
@@ -234,11 +232,10 @@ export default function Search() {
                         </p>
                     )}
 
-                    {!loading &&
-                        listings &&
-                        listings.map((listing) => (
-                            <ListingItem key={listing._id} listing={listing} />
-                        ))}
+                    {!loading && Array.isArray(listings) && listings.map((listing) => (
+                        <ListingItem key={listing._id} listing={listing} />
+                    ))}
+
 
                     {showMore && (
                         <button
